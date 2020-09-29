4d1b67093721d7f4e05be7c2da0f101d30163f85
GugaBaratashvili <gugabaratashvili@gmail.com>
2020-09-29 11:17:03 +0400

    thrird

--- za40/za40/settings.py
+++ za40/za40/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'za40/static')
 ]
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
